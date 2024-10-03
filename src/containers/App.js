f317f1ea480c5763f581b5ff1f49cbcb4e236a28
LTNhan2502 <firstunio@gmail.com>
2024-10-03 11:16:45 +0700

    remove: remove other branch

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,18 +2,13 @@ import React from "react";
 import "./App.scss";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Nav from "../components/Navigation/Nav";
-import { Outlet } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <div className="navigation">
-          <Nav/>
-        </div>
-        <div className="content">
-          <Outlet/>
+        <div>
+          This is main
         </div>
 
         {/* Toastify dùng để hiển thị thông báo */}
