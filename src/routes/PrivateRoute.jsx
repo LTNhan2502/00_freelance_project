0a1025a96c2b6e069acf12422cb5addcbd478775
LTNhan2502 <firstunio@gmail.com>
2024-10-07 09:11:40 +0700

    update: delete trash files

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
      // Kiểm tra xem có access token hay không
-    const isAuthenticated = !!localStorage.getItem("access_token");
+    const isAuthenticated = localStorage.getItem("access_token");
 
     return isAuthenticated ? children : <Navigate to="/login" />;
 };
