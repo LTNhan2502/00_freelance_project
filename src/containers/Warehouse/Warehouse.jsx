dfc416415c8909d32e22fc24497c883e4c304a8f
LTNhan2502 <firstunio@gmail.com>
2024-10-08 15:58:04 +0700

    update: warehouse

--- src/containers/Warehouse/Warehouse.jsx
+++ src/containers/Warehouse/Warehouse.jsx
@@ -3,6 +3,7 @@ import { Card, Col, Container, Row, Table } from 'react-bootstrap'
 import './Warehouse.scss';
 
 function Warehouse() {
+    const userName = localStorage.getItem("user_name")
     const [savedProduct, setSavedProduct] = useState(null);
 
     useEffect(() => {
@@ -12,34 +13,62 @@ function Warehouse() {
         }
     }, []);
     return (
-        <Container className='mt-1 py-5 warehouse-container'>
-            <Row className="h-100 mt-3 justify-content-center align-items-center">
-                <Col lg={6} className="mb-4"> 
-                    <Card className="h-100 blur-card justify-content-center align-items-center">
-                        <Card.Body>
-                            {savedProduct ? (
-                                <>
-                                    <h4>Thông tin sản phẩm đã nhận</h4>
-                                    <Card key={savedProduct.id} className="m-2 received-product-card" style={{ display: 'flex', flexDirection: 'row', width: '30rem' }}>
-                                        <Card.Img 
-                                            variant="left" 
-                                            src={savedProduct.image} 
-                                            style={{ width: '150px', height: '150px', objectFit: 'cover' }} // Đặt chiều cao và chiều rộng cố định
-                                        />
-                                        <Card.Body>
-                                            <Card.Title>{savedProduct.name}</Card.Title>
-                                            <Card.Text>Đơn giá: {savedProduct.price.toFixed(2)} €</Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                </>
-                            ) : (
-                                <Card.Title>Chưa nhận sản phẩm nào</Card.Title>
-                            )}
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+        <Container className="mt-1 py-5 warehouse-container">
+  <Row className="h-100 mt-3 justify-content-center align-items-center">
+    <Col lg={6} className="mb-4">
+      <Card className="h-100 blur-card justify-content-center align-items-center w-100">
+        <Card.Body>
+          {savedProduct ? (
+            <>
+              <h4>Thông tin sản phẩm đã nhận</h4>
+              <Card
+                key={savedProduct.id}
+                className="m-2 received-product-card"
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "30rem",
+                }}
+              >
+                <Card.Img
+                  variant="left"
+                  src={savedProduct.image}
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    objectFit: "cover",
+                  }}
+                />
+                <Card.Body className='warehouse-info'>
+                  <Card.Title>{savedProduct.name}</Card.Title>
+                  <Row>
+                    <Col>
+                      <Card.Text>{savedProduct.price.toFixed(2)} €</Card.Text>
+                    </Col>
+                    <Col className="text-end">
+                      <Card.Text>X6</Card.Text>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Card.Text>Tổng tiền phân phối</Card.Text>
+                    </Col>
+                    <Col className="text-end">
+                      <Card.Text>100 €</Card.Text>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </>
+          ) : (
+            <Card.Title>Chưa nhận sản phẩm nào</Card.Title>
+          )}
+        </Card.Body>
+      </Card>
+    </Col>
+  </Row>
+</Container>
+
     )
 }
 
